99535ca2de5ffd966860aa5fc7ec76dad7721be5
Timothy Eiffe <timothy@kaotech.co>
2021-07-06 15:21:09 +0930

    Added Includes

--- study/c02/ex01/ft_strncpy.c
+++ study/c02/ex01/ft_strncpy.c
@@ -1,4 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
+	int	i;
 
+	i = 0;
+	while (*src[i] != '\0' && *src[i] < n)
+	{
+		*dest[i] = *src[i];
+		i++;
+	}
 }
