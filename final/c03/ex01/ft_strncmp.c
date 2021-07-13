99a2a3159a42120949c7488c3ecac97561566b3e
Timothy Eiffe <timothy@kaotech.co>
2021-07-13 11:42:09 +0930

    first commit

---
+++ final/c03/ex01/ft_strncmp.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while ((s1[i] != '\0') || (s2[i] != '\0'))
+	{
+		j = i;
+		while (j < n)
+		{
+			if (s1[i] < s2[i])
+			{
+				return (-1);
+			}
+			if (s1[i] > s2[i])
+			{
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
---
+++ final/c03/ex03/ft_strncat.c
@@ -0,0 +1,22 @@
+#include <string.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while ((src[j] != '\0') && (j < nb))
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
